5e4226ce4553c217f28afd843ff8a81c83c987c5
vituszove <vituszove@gmail.com>
2021-02-03 03:14:33 +0800

    Updates

---
+++ precache-manifest.211662fe21e70d81b9dbd903c3d65263.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e2006ecb7e548267132788232bf602",
+    "url": "/MaterializeCSS/index.html"
+  },
+  {
+    "revision": "45cc0b2b431eafc8a423",
+    "url": "/MaterializeCSS/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "7ee171ca69dbe676b57c",
+    "url": "/MaterializeCSS/static/css/main.a2d96496.chunk.css"
+  },
+  {
+    "revision": "45cc0b2b431eafc8a423",
+    "url": "/MaterializeCSS/static/js/2.a8dd3ffb.chunk.js"
+  },
+  {
+    "revision": "7ee171ca69dbe676b57c",
+    "url": "/MaterializeCSS/static/js/main.c856d10b.chunk.js"
+  },
+  {
+    "revision": "5e5ae778d7ae8fddf828",
+    "url": "/MaterializeCSS/static/js/runtime-main.e0be17aa.js"
+  },
+  {
+    "revision": "dace51682a0009ca8de47c676d7c6a1c",
+    "url": "/MaterializeCSS/static/media/header.dace5168.png"
+  },
+  {
+    "revision": "edd6b4c56b6d623ae69067ff70ec6ada",
+    "url": "/MaterializeCSS/static/media/parallaxone.edd6b4c5.jpg"
+  },
+  {
+    "revision": "e8668609e2488425d03e9a492fdda9d6",
+    "url": "/MaterializeCSS/static/media/photo1.e8668609.png"
+  }
+]);
